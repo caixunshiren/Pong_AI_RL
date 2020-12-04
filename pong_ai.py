f46060926c10e4afb1d79539f33cc21007c26d9f
caixunshiren <caixunshiren@gmail.com>
2020-12-04 18:16:48 -0500

    1

--- pong_ai.py
+++ pong_ai.py
@@ -1,21 +1,33 @@
+'''
+THE FRANKENSTEIN BOT
+BY JACK CAI & BRIAN CHEN
+
+This bot is a combination of the closed form solution to the position prediction, the chaser ai, and reinforcement learning for optimizing the best aim.
+
+The NN for the reinforcement learning:
+Input: 8 position data from 4 frames = 32
+Output: P(up) for the bot
+        Input: 32   -->RELU-->    layer 1: 200 neurons    -->RELU-->        layer 2: 200 neurons     -->sigmoid-->      P(up)
+
+side note: We know that 200 neurons for both layers are way too many but its too late to change it xxD --- and it works (in many cases)!
+'''
+############## code ###############
 import math
 
-
+# some useful global variables
 ball_pos_history = [(200,200), (202,200), (204,200)] # [(x, y), (x,y) ..
-
-
 predicted_pos = 133+7
-
 #cache = {"v":[], "n":0, "d1":0, "a":0, "p1":0, "p2":0, "table_size":0, "case":0 }
-
 state = "new_game"
 side = "left"
 
 def reinit():
+    #reinitialize the bot
     global ball_pos_history
     ball_pos_history = [(200,200), (202,200), (204,200)]
 
 def check_side(paddle):
+    #checking side
     global side
     if paddle.pos[0] < 150:
         side = "left"
@@ -23,10 +35,11 @@ def check_side(paddle):
         side = "right"
 
 def get_velocity(p1, p2):
+    #computing velocity given two points
     return ((p2[0]-p1[0], p2[1]-p1[1]))
 
-
 def if_flip(ph):
+    #checking at what instance the ball flips
     return (get_velocity(ph[-2], ph[-3])[0] < 0 and get_velocity(ph[-3], ph[-4])[0] > 0) or (get_velocity(ph[-2], ph[-3])[0] > 0 and get_velocity(ph[-3], ph[-4])[0] < 0)
 
 def predict_position(p1, p2, table_size, h):
@@ -127,15 +140,16 @@ def predict_position(p1, p2, table_size, h):
             return 7.5 + a*(abs(v[1])/v[0])
 
     except:
-        print("exception")
+        #print("exception")
         return predicted_pos
 
 def get_sqr_dist(a, b):
+    #find sqr distance between two coords
     return (a.pos[0]-b.pos[0])**2 + (a.pos[1]-b.pos[1])**2
 
 def check_state(paddle_frect, other_paddle_frect, ball_frect):
+    #checking which the ai should be in
     global state
-    #print("flip")
     if get_sqr_dist(paddle_frect, ball_frect) < get_sqr_dist(other_paddle_frect, ball_frect):
         state = "chaser_mode"
         #print("switched to:",state)
@@ -145,6 +159,7 @@ def check_state(paddle_frect, other_paddle_frect, ball_frect):
         #print("switched to:",state)
 
 def check_win(paddle_frect, other_paddle_frect, ball_frect):
+    #check if a bot wins
     global ball_pos_history
     global state
     if paddle_frect.pos[0] > ball_frect.pos[0] and paddle_frect.pos[0] < 100:
@@ -167,72 +182,48 @@ def check_win(paddle_frect, other_paddle_frect, ball_frect):
         state = "new_game"
 
 def print_cache():
+    #prints cache for debugging
     global predicted_pos
     print(cache)
     print(predicted_pos)
 
-def pongbot(paddle_frect, other_paddle_frect, ball_frect, table_size, score):
-    global ball_pos_history # wish we had classes
+def pong_ai(paddle_frect, other_paddle_frect, ball_frect, table_size):
+    #The main body of the AI
+    global ball_pos_history
     global predicted_pos
     global state
-    '''return "up" or "down", depending on which way the paddle should go to
-    align its centre with the centre of the ball, assuming the ball will
-    not be moving
 
-    Arguments:
-    paddle_frect: a rectangle representing the coordinates of the paddle
-                  paddle_frect.pos[0], paddle_frect.pos[1] is the top-left
-                  corner of the rectangle.
-                  paddle_frect.size[0], paddle_frect.size[1] are the dimensions
-                  of the paddle along the x and y axis, respectively
-
-    other_paddle_frect:
-                  a rectangle representing the opponent paddle. It is formatted
-                  in the same way as paddle_frect
-    ball_frect:   a rectangle representing the ball. It is formatted in the
-                  same way as paddle_frect
-    table_size:   table_size[0], table_size[1] are the dimensions of the table,
-                  along the x and the y axis respectively
-
-    The coordinates look as follows:
-
-     0             x
-     |------------->
-     |
-     |
-     |
- y   v
-    '''
-
+    #store nessacerry info
     store_frame_info_more_frames(paddle_frect, other_paddle_frect, ball_frect, 4, table_size)
-
     ball_pos_history.append(ball_frect.pos)
 
+    #check states
     check_side(paddle_frect)
 
-    if if_flip(ball_pos_history): # could make slightly faster by calculating get_velocity in outside loop and passing v to func instead
-        #print("flip!")
+    if if_flip(ball_pos_history):
 
         check_state(paddle_frect, other_paddle_frect, ball_frect)
 
-        v = get_velocity(ball_pos_history[-2], ball_pos_history[-1]) # -'ve x velocity means going to the left
+        v = get_velocity(ball_pos_history[-2], ball_pos_history[-1])
         # update predicted_pos only when opponent hits the ball
         if (((v[0] < 0) and (paddle_frect.pos[0] < table_size[0]/2)) or ((v[0]>0) and (paddle_frect.pos[0]>table_size[0]/2))):
             predicted_pos = predict_position(ball_pos_history[-2], ball_pos_history[-1], table_size, ball_pos_history[-3][1])
 
     check_win(paddle_frect, other_paddle_frect, ball_frect)
 
+    #update according to states
     if state == "chaser_mode" or state == "new_game":
         # do one round of training
         update(True)
-        return pong_ai(paddle_frect, other_paddle_frect, ball_frect, table_size)
+        return chaser(paddle_frect, other_paddle_frect, ball_frect, table_size)
 
     #use chaser if y velocity is too high:
     vy = abs(get_velocity(ball_pos_history[-2], ball_pos_history[-1])[1])
 
-    return controller(predicted_pos, paddle_frect.pos[1], paddle_frect, other_paddle_frect, ball_frect, table_size, score, vy)
+    return controller(predicted_pos, paddle_frect.pos[1], paddle_frect, other_paddle_frect, ball_frect, table_size, vy)
 
-def controller(desired_pos, current_pos, paddle_frect, other_paddle_frect, ball_frect, table_size, score, vy):
+def controller(desired_pos, current_pos, paddle_frect, other_paddle_frect, ball_frect, table_size, vy):
+    #controls the bot --- under prediction mode
     threshold = 7
     if vy < threshold:
         if 35 > abs(desired_pos-35 - current_pos):
@@ -248,29 +239,27 @@ def controller(desired_pos, current_pos, paddle_frect, other_paddle_frect, ball_
     else:
         update()
         if 30 > abs(desired_pos-35 - current_pos):
-            return pong_ai(paddle_frect, other_paddle_frect, ball_frect, table_size)
+            return chaser(paddle_frect, other_paddle_frect, ball_frect, table_size)
         else:
             if current_pos < desired_pos-35:
                 return "down"
             else:
                 return "up"
 
-def pong_ai(paddle_frect, other_paddle_frect, ball_frect, table_size):
-
+def chaser(paddle_frect, other_paddle_frect, ball_frect, table_size):
+    #controls the bot --- under chaser mode
     if paddle_frect.pos[1]+paddle_frect.size[1]/2 < ball_frect.pos[1]+ball_frect.size[1]/2:
      return "down"
     else:
      return "up"
 
-################# from RLbot ###################
+################# from Reinforcement Learning ###################
 
 import numpy as np
 import copy
 
 frame = 1
-last_score = [0,0]
 reset = False
-cumulative_frame = 0
 
 #frame_info
 '''
@@ -329,7 +318,11 @@ def store_frame_info_more_frames(paddle_frect, other_paddle_frect, ball_frect, n
 
 
 def sigmoid(x):
-  return 1.0 / (1.0 + np.exp(-x))
+    try:
+        return 1.0 / (1.0 + np.exp(-x))
+    except:
+        print("exception sigmoid")
+        return np.random.uniform()
 
 def forward_prop(x):
     global side
